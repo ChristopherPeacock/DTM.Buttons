24ca6bc04a569f0309939f51ac183b9e6002d5cc
ChristopherPeacock <peacockchristopher15@gmail.com>
2026-04-19 13:50:45 +0100

    Initial commit: DTM.Buttons class library with ButtonController

---
+++ DTM.Buttons/ButtonController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Diagnostics;
+using Iot.Device.Button;
+
+namespace DTM.Buttons
+{
+    public delegate void ButtonEventHandler(object sender, EventArgs e);
+
+    public class ButtonController
+    {
+        private readonly GpioButton _button;
+
+        public event ButtonEventHandler ButtonPressed;
+        public event ButtonEventHandler ButtonDoublePressed;
+        public event ButtonEventHandler ButtonHeld;
+
+        public ButtonController(int pin)
+        {
+            _button = new GpioButton(buttonPin: pin);
+            _button.IsDoublePressEnabled = true;
+            _button.IsHoldingEnabled = true;
+
+            _button.Press += OnPress;
+            _button.DoublePress += OnDoublePress;
+            _button.Holding += OnHolding;
+        }
+
+        private void OnPress(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Button Pressed!");
+            ButtonPressed?.Invoke(this, e);
+        }
+
+        private void OnDoublePress(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Button Double Pressed!");
+            ButtonDoublePressed?.Invoke(this, e);
+        }
+
+        private void OnHolding(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Button Held!");
+            ButtonHeld?.Invoke(this, e);
+        }
+    }
+}
---
+++ DTM.Buttons/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DTM.Buttons")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DTM.Buttons")]
+[assembly: AssemblyCopyright("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
